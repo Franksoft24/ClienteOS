71e966d7389300a1a41efb884e9610efff890ea2
Franksoft <frankmer95@gmail.com>
2014-10-21 16:02:48 -0400

    Plantilla 1.0 terminada

--- PHP/Registro.php
+++ PHP/Registro.php
@@ -7,32 +7,32 @@
     <meta name="keywords" content="Viaje">
     <meta name="keywords" content="Winter travel">
     <meta name="keywords" content="Viaje de invierno">
-    <meta name="keywords" content="ChapiTravel">
-    <title>Chapi Travel</title>
+    <meta name="keywords" content="ChapiTravel-registro">
+    <title>Chapi Travel / Registro</title>
     <link rel="icon" href="Image/Favicon/favicon.png">
-    <link rel="stylesheet" type="text/css" href="Css/Reset.css">
-    <link rel="stylesheet" type="text/css" href="Css/Style.css">
+    <link rel="stylesheet" type="text/css" href="../Css/Reset.css">
+    <link rel="stylesheet" type="text/css" href="../Css/Style.css">
     <link href='http://fonts.googleapis.com/css?family=Roboto|Lobster' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
-    <script type="text/javascript" src="Script/Posts.js"></script>
-    <script type="text/javascript" src="Script/Script.js"></script>
+    <script type="text/javascript" src="../Script/Posts.js"></script>
+    <script type="text/javascript" src="../Script/Script.js"></script>
 </head>
 <body>
 	<nav>
     	<ul>
         	<a href="#"><li>Acceder</li></a>
-            <a href="PHP/Registro.php"><li>Registrarse</li></a>
+            <a href="#"><li>Registrarse</li></a>
         </ul>
     </nav>
 	<header>
-    	<div id="logo"><img src="Image/chappi travels.png" alt="logo.png" width="100%"></div>
+    	<div id="logo"><img src="../Image/chappi travels.png" alt="logo.png" width="100%"></div>
         <div id="social">
         	<ul>
-            	<a href="#"><li><img alt="facebook.svg" src="Image/svg/facebook7.svg" width="25px"></li></a>
-            	<a href="#"><li><img alt="twitter.svg" src="Image/svg/social71.svg" width="25px"></li></a>
+            	<a href="#"><li><img alt="facebook.svg" src="../Image/svg/facebook7.svg" width="25px"></li></a>
+            	<a href="#"><li><img alt="twitter.svg" src="../Image/svg/social71.svg" width="25px"></li></a>
             </ul>
         </div>
-        <h1><a href="Index.php">Chapi Travel</a></h1>
+        <h1><a href="../Index.php">Chapi Travel</a></h1>
     </header>
     <nav id="menu_secundario">
     	<ul class="nav">
@@ -44,21 +44,21 @@
     </nav>
     <section id="intro">
     	<div class="intro">
-        	<center><div class="img-intro"><center><img alt="avion.png" width="100%" src="Image/New folder/Avion.jpg"></center></div></center>
+        	<center><div class="img-intro"><center><img alt="avion.png" width="100%" src="../Image/New folder/Avion.jpg"></center></div></center>
             <h2>Viajes internacionales</h2>
             <div class="content-intro">
             	<p>Viaje a cualquier parte del mundo con nosotros. Y aprobeche de la más comoda tarifa del mercado que le vamos a ofrcer.</p>
             </div>
         </div>
         <div class="intro">
-        	<center><div class="img-intro"><center><img alt="bus-.png" width="100%" src="Image/New folder/bus-.jpg"></center></div></center>
+        	<center><div class="img-intro"><center><img alt="bus-.png" width="100%" src="../Image/New folder/bus-.jpg"></center></div></center>
             <h2>Giras y excurciones</h2>
             <div class="content-intro">
             	<p>Viaje a cualquier parte del pais con nosotros. Y aprobeche de la más comoda tarifa del mercado que le vamos a ofrcer.</p>
             </div>
         </div>
         <div class="intro">
-        	<center><div class="img-intro"><img alt="varco.png" width="100%" src="Image/New folder/varco.jpg"></div></center>
+        	<center><div class="img-intro"><img alt="varco.png" width="100%" src="../Image/New folder/varco.jpg"></div></center>
             <h2>Cruseros</h2>
             <div class="content-intro">
             	<p>Viaje a cualquier parte del mundo con nosotros. Y aprobeche de la más comoda tarifa del mercado que le vamos a ofrcer.</p>
@@ -66,9 +66,7 @@
         </div>
     </section>
     <section id="post_area">
-    	<script type="text/javascript">
-        	publicaciones();
-        </script>
+    	
     </section>
     <footer>
     	<h6>Desarrollado por chapisoft.</h6>
