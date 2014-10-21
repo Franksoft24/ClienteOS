262dab208838aaf04a046eb656d334fc5c2fe308
Franksoft <frankmer95@gmail.com>
2014-10-21 00:36:35 -0400

    Adiccion de nuevos archivos para el diseño del index del web site

--- Index.php
+++ Index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE HTML>
+<html lang="ES-es, EN-us">
 <head>
 	<meta charset="utf-8">
     <meta name="description" content="this is a travel agence.">
@@ -7,11 +8,28 @@
     <meta name="keywords" content="Winter travel">
     <meta name="keywords" content="Viaje de invierno">
     <meta name="keywords" content="ChapiTravel">
-    <title>Chapi travel</title>
-    <link rel="stylesheet" type="text/css" href="Reset.css">
+    <title>Chapi Travel</title>
+    <link rel="icon" href="Image/Favicon/favicon.png">
+    <link rel="stylesheet" type="text/css" href="Css/Reset.css">
     <link rel="stylesheet" type="text/css" href="Css/Style.css">
-    <link rel="stylesheet" type="text/css" href="Css/Fonts.css">
+    <link href='http://fonts.googleapis.com/css?family=Roboto|Lobster' rel='stylesheet' type='text/css'>
+    <!--link rel="stylesheet" type="text/css" href="Css/Fonts.css"-->
     <script type="text/javascript" src="Script/Script.js"></script>
 </head>
 <body>
+	<nav>
+    	<ul>
+        	<a href="#"><li>Acceder</li></a>
+            <a href="#"><li>Registrarse</li></a>
+        </ul>
+    </nav>
+	<header>
+    	<div id="logo"><img src="Image/chappi travels.png" alt="logo.png" height="100%" width="100%"></div>
+        <h1><a href="Index.php">Chapi Travel</a></h1>
+        <div id="social"></div>
+    </header>
+    <section></section>
+    <aside></aside>
+    <footer></footer>
 </body>
+</html>
